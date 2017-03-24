fde6aa92a1d8d4526b1d3e6c6c1bc3de49a608e3
Antipodas <pedro.algaba@gmail.com>
2017-03-24 11:13:22 +0100

    First commit

---
+++ src/main/java/com/pedro/infinispan/controller/CacheController.java
@@ -0,0 +1,43 @@
+package com.pedro.infinispan.controller;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.infinispan.repository.CustomerRepo;
+import com.pedro.inifinispan.model.Customer;
+
+@RestController
+public class CacheController {
+    @Autowired
+    CustomerRepo custRepo;
+     
+    @RequestMapping("/findCustById")
+    public Customer findCustById(@RequestParam("id") long id){
+        return custRepo.findByCode(id);
+    }
+ 
+    @RequestMapping("/putCust")
+    public String putCustomer(@RequestParam("custNumber") int custNumber){
+        Random r = new Random();
+        for(int i=0; i<custNumber; i++){
+            custRepo.putCustomerToCache(r.nextLong());
+        }
+        return "Done";
+    }
+     
+    @RequestMapping("/evictAll")
+    public String evictAll(){
+        custRepo.evictAllEntries();
+        return "Done";
+    }
+     
+    @RequestMapping("/evictEntry")
+    public String evictEntry(@RequestParam("id") long id){
+        custRepo.evictEntry(id);
+        return "Done";
+    }
+}
